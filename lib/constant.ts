7c277dfa6b418fcab0d319e1acf6527e1d98d784
asczyk <aron.scheffczyk@gmail.com>
2025-10-03 14:33:15 +0200

    add customer data

--- lib/constant.ts
+++ lib/constant.ts
@@ -4,3 +4,54 @@ export const CustomerData = {
   firstName: "Sworn",
   lastName: "Swede",
 };
+
+const customer = [
+  {
+    companyLogo: "ff.png",
+    profileLogo: "sworn.png",
+    firstName: "Sworn",
+    lastName: "Swede",
+  },
+  {
+    firstName: "Hans-Jürgen",
+    lastName: "Reppe",
+    profileLogo: "hans-reppe.png",
+    companyLogo: "reppe.png",
+  },
+  {
+    firstName: "Inna",
+    lastName: "Lucchetti",
+    profileLogo: "inna_volksbank.png",
+    companyLogo: "volksbank.png",
+  },
+  {
+    firstName: "Maha",
+    lastName: "Kurt",
+    profileLogo: "maha_lbs.png",
+    companyLogo: "lbs.png",
+  },
+  {
+    firstName: "Eric",
+    lastName: "Reuter",
+    profileLogo: "reuter.png",
+    companyLogo: "eric_reuter.png",
+  },
+  {
+    firstName: "Daniel",
+    lastName: "Törteli",
+    profileLogo: "daniel_toerteli.png",
+    companyLogo: "toerteli.png",
+  },
+  {
+    firstName: "Thorsten",
+    lastName: "Rösner",
+    profileLogo: "thorsten_juno.png",
+    companyLogo: "juno.png",
+  },
+  {
+    firstName: "Oliver",
+    lastName: "Margraf",
+    profileLogo: "oliver_margraf.png",
+    companyLogo: "margraf.png",
+  },
+];
